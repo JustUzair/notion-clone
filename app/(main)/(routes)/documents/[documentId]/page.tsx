82207614e6ab51cff94cbd39ee8bdd720d1d5408
JustUzair <uzairhajra763390@gmail.com>
2023-11-06 00:10:12 +0530

    Added toolbar and iconpicker for notes

--- app/(main)/(routes)/documents/[documentId]/page.tsx
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -1,5 +1,34 @@
-const DocumentIdPage = () => {
-  return <div>DocumentId</div>;
+"use client";
+
+import { api } from "@/convex/_generated/api";
+import { useQuery } from "convex/react";
+import { Id } from "@/convex/_generated/dataModel";
+import { Toolbar } from "@/components/toolbar";
+interface DocIdPageProps {
+  params: {
+    documentId: Id<"documents">;
+  };
+}
+const DocumentIdPage = ({ params }: DocIdPageProps) => {
+  const document = useQuery(api.documents.getById, {
+    documentId: params.documentId,
+  });
+
+  if (document === undefined) {
+    return <>Loading</>;
+  }
+
+  if (document === null) {
+    return <>Not Found</>;
+  }
+  return (
+    <div className="pb-40">
+      <div className="h-[35vh]" />
+      <div className="md:max-w-3xl lg:max-w-4xl mx-auto">
+        <Toolbar initialData={document} />
+      </div>
+    </div>
+  );
 };
 
 export default DocumentIdPage;
--- app/(main)/_components/navbar.tsx
+++ app/(main)/_components/navbar.tsx
@@ -19,7 +19,9 @@ export const Navbar = ({ isCollapsed, onResetWidth }: Props) => {
   const document = useQuery(api.documents.getById, {
     documentId: params?.documentId as Id<"documents">,
   });
-
+  if (document === null) {
+    return null;
+  }
   if (document === undefined) {
     return (
       <nav className="bg-background dark:bg-[#1f1f1f] px-3 py-2 w-full flex items-center justify-between">
@@ -30,9 +32,7 @@ export const Navbar = ({ isCollapsed, onResetWidth }: Props) => {
       </nav>
     );
   }
-  if (document === null) {
-    return null;
-  }
+
   return (
     <>
       <nav className="bg-background dark:bg-[#1f1f1f] px-3 py-2 w-full flex items-center gap-x-4">
